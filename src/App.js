409abfc9fcfe7bbc7c5ec65ef89331bdc3803bce
David Nordeen <davidnordeen@gmail.com>
2019-04-15 11:28:30 -0700

    finished #6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Split from './composition/Split';
+import './App.css';
 
 function App() {
   return (
     <main className='App'>
-      <Split className='left' flexBasis='2'>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+      <Split className='left' flexBasis={2}>
+        This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
       </Split>
       <Split className='right'>
-        Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
+        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
       </Split>
     </main>
   )
